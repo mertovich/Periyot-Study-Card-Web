5b6996d4eec5d0599d0ea78753d774f19c7e93c6
mertovich <88425310+mertovich@users.noreply.github.com>
2022-02-24 14:49:56 +0300

    add(BottomMenu)

--- web/src/components/BottomMenu.js
+++ web/src/components/BottomMenu.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import {Col, Container } from 'react-bootstrap'
+import '../App.css'
 
-const BottomMenu = () => {
+const BottomMenu = props => {
   return (
-    <div>BottomMenu</div>
+    <div>
+      <Container className='text-center'>
+        <Col>
+        <button onClick={()=> props.p1()} className={props.selectPage === 0 ? 'bottom-menu-btn-active' :'bottom-menu-btn'}></button>
+        <button onClick={()=> props.p2()} className={props.selectPage === 1 ? 'bottom-menu-btn-active' :'bottom-menu-btn'}></button>
+        <button onClick={()=> props.p3()} className={props.selectPage === 2 ? 'bottom-menu-btn-active' :'bottom-menu-btn'}></button>
+        </Col>
+      </Container>
+    </div>
   )
 }
 
--- web/src/pages/Home.js
+++ web/src/pages/Home.js
@@ -5,13 +5,24 @@ import P2 from '../components/P2'
 import P3 from '../components/P3'
 
 const Home = () => {
-    const [selectPage, setselectPage] = useState(2)
+    const [selectPage, setselectPage] = useState(0)
+
+    const p1 = () =>{
+        setselectPage(0)
+    }
+    const p2 = () =>{
+        setselectPage(1)
+    }
+    const p3 = () =>{
+        setselectPage(2)
+    }
+
     return (
         <div>
             {selectPage === 0 ? <P1 /> : null}
             {selectPage === 1 ? <P2 /> : null}
             {selectPage === 2 ? <P3 /> : null}
-            <BottomMenu/>
+            <BottomMenu selectPage={selectPage} p1={p1} p2={p2} p3={p3} />
         </div>
     )
 }
